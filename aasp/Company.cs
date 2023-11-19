b6e19c6b450753247db81e6d0bc6b3277a602792
SofaL0rd <nazzar2303@gmail.com>
2023-11-20 01:33:18 +0200

    LR 2

--- aasp/Company.cs
+++ aasp/Company.cs
@@ -1,23 +1,5 @@
 ﻿public class Company
 {
-    private readonly RequestDelegate next;
-    private readonly string name;
-    private readonly int yearFounded;
-
-    public Company(RequestDelegate next, string name, int yearFounded)
-    {
-        this.next = next;
-        this.name = name;
-        this.yearFounded = yearFounded;
-    }
-
-    public async Task InvokeAsync(HttpContext context)
-    {
-        context.Response.ContentType = "text/plain";
-        var random = new Random();
-        var randNum = random.Next(0, 101);
-
-        await context.Response.WriteAsync($"Random number: {randNum}\n");
-        await context.Response.WriteAsync($"Company:{name} , Year Founded:{yearFounded} ");
-    }
+    public string Name { get; set; } = "";
+    public int Employees { get; set; } = 0;
 }
--- aasp/Program.cs
+++ aasp/Program.cs
@@ -1,7 +1,51 @@
-var builder = WebApplication.CreateBuilder();
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-app.UseMiddleware<Company>("Skynet", 1988);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Додайте конфігурацію
+        builder.Configuration
+            .AddJsonFile("companyConfig.json")
+            .AddXmlFile("companyConfig.xml")
+            .AddIniFile("companyConfig.ini")
+            .AddJsonFile("myInfoConfig.json");
+
+        // Додайте сервіси
+        builder.Services.AddSingleton<CompanyService>();
+
+        var app = builder.Build();
+
+        // Виведіть дані про компанії в консоль та в браузер
+        var companyService = app.Services.GetRequiredService<CompanyService>();
+        companyService.DisplayMostEmployedCompany();
+
+        // Виведіть дані про вас у вікно браузера
+        app.Map("/", () =>
+        {
+            var myInfo = builder.Configuration.GetSection("MyInfo").Get<MyInfo>();
+            var company = companyService.GetMostEmployedCompany();
+
+            return "My Information:" +
+                   $" Name: {myInfo.Name}" +
+                   $" Age: {myInfo.Age}" +
+                   $" Occupation: {myInfo.Occupation}" +
+                   $" Most Employed Company" +
+                   $" Name: {company.Name}" +
+                   $" Employees: {company.Employees}";
+        });
+
+        app.Run();
+    }
+}
 
 
-app.Run();
+
+
+
