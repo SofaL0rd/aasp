1536069a6a073d10645c9f92c32ea7e10ae13996
SofaL0rd <nazzar2303@gmail.com>
2023-12-08 01:45:10 +0200

    Lr5

--- aasp/Program.cs
+++ aasp/Program.cs
@@ -1,66 +1,53 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Text;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
         var builder = WebApplication.CreateBuilder(args);
-        builder.Configuration
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("library_config.json", optional: false, reloadOnChange: true);
-        var app = builder.Build();
+        builder.Services.AddLogging();
 
-        app.Map("/Library", LibraryHandler);
-        app.Map("/Library/Books", BooksHandler);
-        app.Map("/Library/Profile/{id?}", ProfileHandler);
+        var app = builder.Build();
+        app.UseStaticFiles();
+
+        app.UseMiddleware<ErrorLoggingMiddleware>();
+
+
+        app.Map("/checkCookie", CheckCookieHandler);
+        app.Map("/", FormHandler);
+
+        app.Map("/error", async (context) =>
+        {
+            throw new Exception();
+
+        });
+
+
+       
+
 
         app.Run();
     }
-    private static void LibraryHandler(HttpContext context)
+   
+    private static void CheckCookieHandler(HttpContext context)
     {
-        context.Response.WriteAsync("Greetings from the library!");
-    }
-
-    private static void BooksHandler(HttpContext context)
-    {
-        var booksConfig = context.RequestServices.GetRequiredService<IConfiguration>().GetSection("Books");
-
-        Console.WriteLine(booksConfig);
-        context.Response.WriteAsync("List of books:\n");
-        foreach (var book in booksConfig.GetChildren())
+        var cookieValue = context.Request.Cookies["cookieValue"];
+        if (cookieValue != null)
         {
-            var name = book["Name"];
-            var author = book["Author"];
-            var yearPublished = book["Year published"];
-
-            context.Response.WriteAsync($"Name: {name}, Author: {author}, Year Published: {yearPublished}\n");
-        }
-    }
-
-    private static void ProfileHandler(HttpContext context)
-    {
-        if (context.Request.RouteValues.TryGetValue("id", out var idObj) && int.TryParse(idObj?.ToString(), out var userId) && userId >= 0 && userId <= 5)
-        {
-            var userInfoConfig = context.RequestServices.GetRequiredService<IConfiguration>().GetSection($"Users:{userId}");
-
-            context.Response.WriteAsync($"User information for id {userId}:\n");
-            context.Response.WriteAsync($"Name: {userInfoConfig["Name"]}\n");
-            context.Response.WriteAsync($"Age: {userInfoConfig["Age"]}\n");
-            context.Response.WriteAsync($"Email: {userInfoConfig["Email"]}\n");
+            context.Response.WriteAsync($"Cookie value: {cookieValue}");
         }
         else
         {
-            var userInfoConfig = context.RequestServices.GetRequiredService<IConfiguration>().GetSection($"Users:self");
-
-            context.Response.WriteAsync($"User information\n");
-            context.Response.WriteAsync($"Name: {userInfoConfig["Name"]}\n");
-            context.Response.WriteAsync($"Age: {userInfoConfig["Age"]}\n");
-            context.Response.WriteAsync($"Email: {userInfoConfig["Email"]}\n");
+            context.Response.WriteAsync("Cookie not found");
         }
+    }
+
+    private static void FormHandler(HttpContext context)
+    {
+        context.Response.Redirect("/index.html");
     }
 }
 
